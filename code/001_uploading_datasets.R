29b26241ba9006541873d67caf4de13e599eaabf
ccardonaandrade <ccardonaandrade@gmail.com>
2024-09-30 10:15:48 -0500

    Creating folders and r-script

---
+++ code/001_uploading_datasets.R
@@ -0,0 +1,13 @@
+## ---------------------------
+## Script name: African Coups and Polity IV
+## Author: Carlos Cardona
+## Date Created: `r paste(Sys.Date())`
+## ---------------------------
+## set working directory
+setwd("C:/Users/ccard/OneDrive/Documentos/GitHub/african_politics")     # Tim's working directory (PC)
+## ---------------------------
+
+## load up the packages we will need:  (uncomment as required)
+
+library(tidyverse)
+library(data.table)
